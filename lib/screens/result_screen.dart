f1d0e6b474c5fe2d68fb89a912a4ba6433001276
RamezRayyan64 <Ramez.Rayyan64@gmail.com>
2023-05-29 16:48:39 +0430

    your result text added to result screen

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -24,7 +24,60 @@ class _ResultScreenState extends State<ResultScreen> {
         centerTitle: true,
       ),
       body: Column(
-        children: [],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Text(
+              "Your Result",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 200,
+              margin: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  color: const Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      result >= 25
+                          ? "OVERWEIGHT"
+                          : result > 18
+                              ? "NORMAL"
+                              : "UNDERWEIGHT",
+                      style: const TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF24D876)),
+                    ),
+                    Text(
+                      result.toStringAsFixed(1),
+                      style: const TextStyle(
+                          fontSize: 100, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        result >= 25
+                            ? "You have a higher than normal body weight. Try to exercize more."
+                            : result > 18
+                                ? "You have a normal body weight. Good job!"
+                                : "You have a lower than normal body weight. You can eat a bit more.",
+                        style: const TextStyle(fontSize: 25),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
